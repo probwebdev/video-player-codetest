b064a9b1f506324db7d3b529dc09c7c1a48da44e
Robert Harbul <robert.harbul@gmail.com>
2023-09-20 14:50:28 +0200

    add tooltip and video

--- apps/tv4-web/pages/index.tsx
+++ apps/tv4-web/pages/index.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 import { Poster } from '~/components/Poster';
+import { Tooltip } from '~/components/Tooltip';
+import { Video } from '~/components/Video';
 import { trpc } from '~/utils/trpc';
 
 const IndexPage = () => {
+  const [currentMovie, setCurrentMovie] = useState<string | undefined>(
+    undefined
+  );
   const { data, error } = trpc.tv4.showcase.useQuery();
 
   if (error?.message) {
@@ -14,14 +21,28 @@ const IndexPage = () => {
 
   return (
     <div className="showcase">
+      <Video src={currentMovie ?? ''} />
       {data?.panels.map(({ title, items }) => (
         <section key={title} className="showcase-section">
           <h2>{title}</h2>
           <ul key={title} className="list">
             {items.map(({ id, title, description, image, video }) => (
-              <li key={id}>
-                <Poster title={title} description={description} image={image} />
-              </li>
+              <Tooltip key={id} text={description}>
+                {/* eslint-disable */}
+                <li
+                  tabIndex={0}
+                  onClick={() => {
+                    setCurrentMovie(video);
+                  }}
+                >
+                  <Poster
+                    title={title}
+                    description={description}
+                    image={image}
+                  />
+                </li>
+                {/* eslint-enable */}
+              </Tooltip>
             ))}
           </ul>
         </section>
